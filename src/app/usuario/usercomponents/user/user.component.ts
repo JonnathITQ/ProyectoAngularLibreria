ce33fe6896e11330907ab49c6364b5c372aeb80c
LeslyBc <lbecerracampana@gmail.com>
2025-12-01 16:54:10 -0500

    mejora pagina principal usuario

--- src/app/usuario/usercomponents/user/user.component.ts
+++ src/app/usuario/usercomponents/user/user.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
